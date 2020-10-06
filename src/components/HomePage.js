6ba44a17b713e70ab2c25b18ba1feaec84d56915
Ushahemba Shir <laleoict@gmail.com>
2020-10-06 16:57:46 +0100

    fist commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Search from './Search';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Search />
+    </div>
+  );
+};
+
+export default HomePage;
